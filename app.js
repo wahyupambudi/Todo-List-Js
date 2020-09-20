deedb560c85b02d54050f90b4233ebe6c4f0a0e8
Wahyu Pambudi <wahyupambudi823@gmail.com>
2020-09-20 08:47:24 +0700

    add app.js

--- app.js
+++ app.js
@@ -17,47 +17,47 @@ const clearButton = document.querySelector("#clear-todos");
 todoForm.addEventListener("submit", addTodo);
 todoList.addEventListener("click", deleteTodo);
 clearButton.addEventListener("click", clearTodos);
+filterInput.addEventListener("keyup", filterTodos);
 
 function addTodo(e) {
   e.preventDefault();
-  
-  if(todoInput.value) {
+
+  if (todoInput.value) {
     // membuat li element
     const li = document.createElement("li");
-  
+
     // menambah class element li
     li.className =
-      "list-group-item d-flex justify-content-between align-items-center mb-1 ";
-  
+      "todo-item list-group-item d-flex justify-content-between align-items-center mb-1 ";
+
     //menambah clihdren ke li
     li.appendChild(document.createTextNode(todoInput.value));
-  
+
     //   Membuat Delete Button a href
     const a = document.createElement("a");
-  
+
     // memberi property untuk a element
     a.href = "#";
     a.className = "badge badge-danger delete-todo";
-  
+
     //   add child
     a.innerHTML = "Delete";
-  
+
     //   menyelipkan elemen a ke dalam childern li
     li.appendChild(a);
-  
+
     //   add element li yang dibuat oleh javascript
     // ke dalam todolist
-  
+
     todoList.appendChild(li);
-  
+
     //   console.log(li);
-  
+
     // mengkosongkan todoinput setelah input data
     todoInput.value = "";
   } else {
-    alert("Todo Tidak Boleh Kosong!!")
+    alert("Todo Tidak Boleh Kosong!!");
   }
-
 }
 
 function deleteTodo(e) {
@@ -76,4 +76,27 @@ function deleteTodo(e) {
 function clearTodos() {
   // menghapus list todo
   todoList.innerHTML = "";
+}
+
+function filterTodos(e) {
+  const filterText = e.target.value.toLowerCase();
+
+  // menyeleksi semua class dari todo-item
+  const todoItems = document.querySelectorAll(".todo-item");
+
+  // menyeleksi dengan foreach
+  todoItems.forEach((item) => {
+    const itemText = item.firstChild.textContent.toLowerCase();
+
+    // jika itemText dari filterText bernilai true
+    if (itemText.indexOf(filterText) !== -1) {
+      // membiarkan elemen tetap ada
+      item.setAttribute("style", "display: block;");
+    } else {
+      // hidden element
+      item.setAttribute("style", "display: none !important;");
+    }
+
+    console.log(itemText);
+  });
 }
